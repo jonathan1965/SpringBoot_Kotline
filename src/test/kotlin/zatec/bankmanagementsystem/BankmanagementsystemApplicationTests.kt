f3dbf014feff0b643a6ded170757c36f4ee77c73
jonathan1965 <jonakarisa@gmail.com>
2022-03-28 00:44:42 +0200

    fisrt commit

---
+++ src/test/kotlin/zatec/bankmanagementsystem/BankmanagementsystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package zatec.bankmanagementsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankmanagementsystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
